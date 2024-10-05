70d5a2afbdd4a608d0587dedcbb089394512de84
David Zhao <dzhao6@gmail.com>
2024-10-05 13:57:03 -0600

    Added another solution to task2
    - solution does not use dictionary given by python

--- general/task2.py
+++ general/task2.py
@@ -1,10 +1,44 @@
 class DataStore():
-    #could be a dictionary or hashmap
-    pass
+    def __init__(self):
+        self.values = []
+        self.keys = []
+
+    def createKey(self, key, value):
+        if(key in self.keys):
+            valueIndex = self.keys.index(key)
+            self.values[valueIndex] = value
+        elif(key not in self.keys):
+            self.keys.append(key)
+            self.values.append(value)
+        else:
+            raise Exception("Issue when creating key")
+        
+    def readKey(self,key):
+        if(key in self.keys):
+            valueIndex = self.keys.index(key)
+            return self.values[valueIndex]
+        else:
+            raise Exception(key + " does not exist as a key: Cannot read")
+        
+    def updateKey(self, key, value):
+            if(key in self.keys):
+                valueIndex = self.keys.index[key]
+                self.values[valueIndex] = value
+            else:
+                raise Exception(key + " does not exist as a key: Cannot update")
+            
+    def deleteKey(self, key):
+        if(key in self.keys):
+            valueIndex = self.keys.index[key]
+            self.keys.remove(key)
+            self.values.pop(valueIndex)
+        else:
+            raise Exception(key + " does not exist as a key: Cannot delete")
+    
 class DataStoreDict(dict):
     def __init__(self, store=dict):
         self.store = store
-    def createKey(self,key, value):
+    def createKey(self, key, value):
         self.store.setdefault(key, value)
     def readKey(self,key):
         return self.store.get(key)
