ae0280df562bdbd3a79a06b91ec79ce173584f30
David Zhao <dzhao6@gmail.com>
2024-10-06 00:42:55 -0600

    Added testers to task2 and task3

--- general/task2.py
+++ general/task2.py
@@ -3,6 +3,12 @@ class DataStore():
         self.values = []
         self.keys = []
 
+    def toString(self):
+        items = ""
+        for i in range(len(self.values)):
+            items += f"{self.keys[i]}: {self.values[i]}, "
+        return items
+    
     def createKey(self, key, value):
         if(key in self.keys):
             valueIndex = self.keys.index(key)
@@ -22,27 +28,65 @@ class DataStore():
         
     def updateKey(self, key, value):
             if(key in self.keys):
-                valueIndex = self.keys.index[key]
+                valueIndex = self.keys.index(key)
                 self.values[valueIndex] = value
             else:
                 raise Exception(f"'{key}' does not exist as a key: Cannot update")
             
     def deleteKey(self, key):
         if(key in self.keys):
-            valueIndex = self.keys.index[key]
+            valueIndex = self.keys.index(key)
             self.keys.remove(key)
+            valueRemoved = self.values[valueIndex]
             self.values.pop(valueIndex)
+            return valueRemoved
         else:
             raise Exception(f"'{key}' does not exist as a key: Cannot delete")
     
 class DataStoreDict(dict):
     def __init__(self, store=dict):
         self.store = store
+
+    def toString(self):
+        return self.store
+    
     def createKey(self, key, value):
         self.store.setdefault(key, value)
+
     def readKey(self,key):
         return self.store.get(key)
+    
     def updateKey(self,key,value):
         self.store[key] = value
+
     def deleteKey(self, key):
         return self.store.pop(key)
+    
+
+if __name__ == "__main__":
+    storage = DataStore()
+    storage.createKey("id", "1")
+    storage.createKey("Job", "Accountant")
+    storage.createKey("Amount", 1)
+    print(storage.toString())
+    storage.createKey("Amount","4")
+    storage.updateKey("Job", "Manager")
+    #storage.updateKey("342",21)
+    print(storage.toString())
+    storage.updateKey("Amount", 2)
+    print(storage.toString())
+    print(storage.readKey("Job"))
+    #print(storage.readKey("1"))
+    print(storage.deleteKey("Amount"))
+    #storage.deleteKey("123")
+    print(storage.toString())
+    dictStorage = DataStoreDict({})
+    dictStorage.createKey("id", 1)
+    dictStorage.createKey("Job", "Construction")
+    dictStorage.createKey("Amount", 1)
+    print(dictStorage.toString())
+    dictStorage.updateKey("Job", "Manager")
+    print(dictStorage.toString())
+    print(dictStorage.readKey("Amount"))
+    print(dictStorage.deleteKey("Job"))
+    print(dictStorage.toString())
--- general/task3.py
+++ general/task3.py
@@ -2,52 +2,62 @@ import task1 as fileToArray
 import csv
 import os
 
-def textFileProcesser(textFile):
-    punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-    inArray = list(textFile.read())
-    for word in inArray:
-        for char in word:
-            if char in punc:
-                word = word.replace(char, "")
+def textFileProcesser(textFile:str):
+    punc = '''!()-[]}{;:'"\,<>./?@#$%^&*_~'''
+    inArray = textFile.read().split(" ")
+    try:
+        for i in range(len(inArray)):
+            inArray[i] = int(inArray[i])
+    except TypeError:
+        for word in inArray:
+            for char in word:
+                if char in punc and word != "n/a":
+                    word = word.replace(char, "")
+    
     data = fileToArray.ArrayHandler(inArray)
     return data.array
 
-def csvFileProcesser(csvFile):
+def csvFileProcesser(csvFile:str):
     data = []
     keys = []
-    csvReader = csv.reader(csvFile)
-    keys = csvReader.readline()
-    if "application_id" in keys:
-        csvDictReader = csv.DictReader(csvFile)
-        for row in csvDictReader:
-            data.append(row)
-    elif "application_id" not in keys:
-        raise Exception("no key for application_id found")
-    else:
-        raise Exception("error when reading csv file")
-    return data
-
-def binaryFileProcesser(binaryFile):
-    inArray = []
-    for i in binaryFile:
-        pass
+    keys = csvFile.readline()
+    csvDictReader = csv.DictReader(csvFile)
+    for row in csvDictReader:
+        data.append(row)
     
-def fileReader(fileName):
+    if "application_id" not in keys:
+        raise Exception("no key for application_id found")
+    elif len(data) < 15:
+        raise Exception(f"csv file has to little rows: {len(data)} < 15")
+    elif len(data) > 50:
+        raise Exception(f"csv file has to many rows: {len(data)} > 50")
+    else:
+        print("data read successfully")
+        return data
+
+def fileReader(fileName:str):
     try:
+        outputFile = []
         _, extention = os.path.splitext(fileName)
         if(extention == ".txt"):
             with open(fileName, "r") as textFile:
-                textFileProcesser(textFile)
+                outputFile = textFileProcesser(textFile)
                 textFile.close()
+                return outputFile
         elif(extention == ".csv"):
             with open(fileName, "r") as csvFile:
-                csvFileProcesser(csvFile)
+                outputFile = csvFileProcesser(csvFile)
                 csvFile.close()
-        elif(extention == ".bin" or extention == ".dat"):
-            with open(fileName, "rb") as binaryFile:
-                binaryFileProcesser(binaryFile)
-                binaryFile.close()
-
-
+                return outputFile
+        else:
+            if extention == "":
+                raise Exception("no extention given")
+            else:
+                raise Exception(f"{extention} is not a file type that can be read")
     except NameError:
-        raise Exception(f"'{fileName}' cannot be found within directory")
+        raise Exception(f"{fileName} was not found within directory")
+
+if __name__ == "__main__":
+    fileToRead = "MBA_good.csv"
+    read = fileReader(f"/Users/davidzhao/Dev/CodingQuestions/general/test_files/{fileToRead}")
+    print(read)
